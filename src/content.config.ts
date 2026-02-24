04b959616faa06d3e463cb698fc5fdd836540b6a
Devesh Chaudhari <DeveshRx@users.noreply.github.com>
2026-02-24 23:36:38 +0530

    update

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,17 +4,19 @@ import { glob, file } from 'astro/loaders';
 
 
 const generalBlog = defineCollection({
-      loader: glob({ pattern: ['*.md', '*.mdx', '!draft-*'], base: 'src/content/' }),
-    schema: z.object({
-        slug: z.string(),
-        title: z.string(),
-        image: z.string(),
-        date: z.coerce.date(),
-        description: z.string(),
-        keywords: z.array(z.string()),
-        relatedPosts: z.array(reference('generalBlog')).optional(),
-        
-    }),
+  loader: glob({
+    pattern: ["*.md", "*.mdx", "!draft-*"],
+    base: "src/content/",
+  }),
+  schema: z.object({
+    slug: z.string(),
+    title: z.string(),
+    image: z.string(),
+    date: z.coerce.date(),
+    description: z.string(),
+    keywords: z.array(z.string()),
+    relatedPosts: z.array(reference("generalBlog")).optional(),
+  }),
 });
 
 
